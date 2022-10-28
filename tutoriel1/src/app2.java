0289ae05f2f8ce720973e41c2c92d0100c9f59e7
sunshay <sunshayina@outlook.com>
2022-10-28 11:44:17 -0700

    do it again

---
+++ tutoriel1/src/app2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class app2 {
+
+    public static void main(String args[]) {
+       
+        System.out.println("tout c ceci est beau");
+        System.out.println("g ne lach rien");
+        String a = new String("tout ceci est parfait");
+        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        String i = sc.next();
+        System.out.println("you entered " + i);
+    }
+    
+}
