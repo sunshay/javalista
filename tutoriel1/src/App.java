b1df86d426ea3c7d672ebfd964ccf4b75b61d4ec
sunshay <sunshayina@outlook.com>
2022-10-28 11:44:03 -0700

    commit a simple java

---
+++ tutoriel1/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.print("how are you doin ?");
+        String a = new String("parse data");
+        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        String i = sc.next();
+        System.out.println("just enter" + i );
+         
+
+
+    }
+}
